f0083e5c25de957ed804cc86fd2f202ed4a9d3a6
Dan Carlton <danlouis.carlton@gmail.com>
2024-07-17 14:47:33 -0700

    static methods

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
 // Basic
 
-class Rectangle {
-    constructor(_width, _height, _balls) {
-      this._width = _width
-      this._height = _height
-      this._balls = _balls
-    }
+// class Rectangle {
+//     constructor(_width, _height, _balls) {
+//       this._width = _width
+//       this._height = _height
+//       this._balls = _balls
+//     }
 
-    getArea() {
-      return this._width * this._height
-    }
+//     getArea() {
+//       return this._width * this._height
+//     }
 
-  }
+//   }
 
-  let rectangle = new Rectangle(4, 8, 2)
-//   console.log(rectangle)
-//   console.log(rectangle.getArea())
+//   let rectangle = new Rectangle(4, 8, 2)
+// //   console.log(rectangle)
+// //   console.log(rectangle.getArea())
 
+// //   Getters & Setters
 
-//   Getters & Setters
+// class Square {
+//     constructor(width){
+//         this._width = width
+//         this._height = width
+//     }
 
-class Square {
-    constructor(width){
-        this._width = width
-        this._height = width
-    }
+//     get area () {
+//         return this._height * this._width
+//     }
 
-    get area () {
-        return this._height * this._width
-    }
+//     set area (area) {
+//         this._width = Math.sqrt(area)
+//         this._height = this._width
+//     }
+// }
 
-    set area (area) {
-        this._width = Math.sqrt(area)
-        this._height = this._width
-    }
-}
+// const square = new Square(4)
+// square.area = 25
 
-const square = new Square(4)
-square.area = 25
-
-console.log(square.area)
-
-
+// console.log(square.area)
 
 //  Static Methods
+class Square {
+  constructor(width) {
+    this._width = width
+    this._height = width
+  }
+
+  static equals(a, b) {
+    return a._width * a._height === b._width * b._height
+  }
+}
+
+const square = new Square(8)
+const squaredUpFoo = new Square(8)
+
+console.log(Square.equals(square, squaredUpFoo))
